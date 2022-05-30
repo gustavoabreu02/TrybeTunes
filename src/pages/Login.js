94b76e571056decc857be8a45be49e6d00949469
Gustavo Abreu <gustavofart371@gmail.com>
2022-05-30 18:05:23 -0300

    Cria um formulário para identificação

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,85 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+import Carregando from './Carregando';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+    this.verificaButtonEntrar = this.verificaButtonEntrar.bind(this);
+    this.state = {
+      name: '',
+      buttonEntrar: true,
+      loading: true,
+    };
+  }
+
+  handleChange(event) {
+    this.verificaButtonEntrar(event);
+    this.setState({
+      name: event.target.value,
+    });
+  }
+
+  verificaButtonEntrar(event) {
+    const nameLength = 3;
+    if (event.target.value.length >= nameLength) {
+      this.setState({
+        buttonEntrar: false,
+      });
+    } else {
+      this.setState({
+        buttonEntrar: true,
+      });
+    }
+  }
+
   render() {
-    console.log(this.props);
+    const { name, buttonEntrar, loading } = this.state;
+    const { history } = this.props;
     return (
-      <div data-testid="page-login">
-        <p>Login</p>
+      <div>
+        {loading ? (
+          <div data-testid="page-login">
+            <form>
+              <label htmlFor="name-input">
+                Nome
+                <input
+                  data-testid="login-name-input"
+                  type="text"
+                  id="name-input"
+                  value={ name }
+                  onChange={ this.handleChange }
+                />
+              </label>
+              <button
+                data-testid="login-submit-button"
+                type="button"
+                disabled={ buttonEntrar }
+                onClick={ () => {
+                  this.setState({
+                    loading: false,
+                  }, async () => {
+                    await createUser(this.state);
+                    history.push('/search');
+                  });
+                } }
+              >
+                Entrar
+              </button>
+            </form>
+          </div>
+        ) : (
+          <Carregando />
+        )}
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.string.isRequired,
+};
 
 export default Login;
