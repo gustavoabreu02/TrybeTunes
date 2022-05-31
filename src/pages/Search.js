ac379d015cb31359acadca69853b3fe04f16e0ef
Gustavo Abreu <gustavofart371@gmail.com>
2022-05-31 13:29:00 -0300

    Faz a requisição para pesquisar artistas

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Carregando from './Carregando';
 
 class Search extends React.Component {
   constructor() {
     super();
     this.handleChange = this.handleChange.bind(this);
     this.buttonPesquisar = this.buttonPesquisar.bind(this);
+    this.requisiçãoAlbuns = this.requisiçãoAlbuns.bind(this);
     this.state = {
       searchInput: '',
       buttonDisabled: true,
+      loading: false,
+      albuns: [],
+      nameAnterior: '',
+      loadingAlbuns: true,
+      loadingComponent: false,
+      nenhumAlbumEncontrado: false,
     };
   }
 
@@ -31,25 +41,85 @@ class Search extends React.Component {
     }
   }
 
+  requisiçãoAlbuns() {
+    const { searchInput } = this.state;
+    this.setState(() => ({
+      loadingAlbuns: true,
+      loadingComponent: true,
+    }), async () => {
+      const resulSearchAlbumAPI = await searchAlbumsAPI(searchInput);
+      console.log(resulSearchAlbumAPI);
+      if (resulSearchAlbumAPI.length === 0) {
+        this.setState({
+          nenhumAlbumEncontrado: true,
+          loadingComponent: false,
+        });
+      } else {
+        this.setState({
+          nenhumAlbumEncontrado: false,
+          loadingComponent: false,
+          loadingAlbuns: false,
+          nameAnterior: searchInput,
+          searchInput: '',
+          albuns: resulSearchAlbumAPI,
+        });
+      }
+    });
+  }
+
   render() {
-    const { searchInput, buttonDisabled } = this.state;
+    const { searchInput,
+      buttonDisabled,
+      loading,
+      loadingAlbuns,
+      albuns,
+      nameAnterior,
+      loadingComponent,
+      nenhumAlbumEncontrado } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <form>
-          <input
-            data-testid="search-artist-input"
-            value={ searchInput }
-            onChange={ this.handleChange }
-          />
-          <button
-            type="button"
-            data-testid="search-artist-button"
-            disabled={ buttonDisabled }
-          >
-            Pesquisar
-          </button>
-        </form>
+        {loading ? (
+          <Carregando />
+        ) : (
+          <div>
+            <form>
+              <input
+                data-testid="search-artist-input"
+                value={ searchInput }
+                onChange={ this.handleChange }
+              />
+              <button
+                type="button"
+                data-testid="search-artist-button"
+                disabled={ buttonDisabled }
+                onClick={ this.requisiçãoAlbuns }
+              >
+                Pesquisar
+              </button>
+            </form>
+          </div>
+        )}
+        {nenhumAlbumEncontrado ? <h1>Nenhum álbum foi encontrado</h1> : ''}
+        {loadingComponent ? <Carregando /> : ''}
+        {loadingAlbuns ? '' : (
+          <div>
+            <h3>{`Resultado de álbuns de: ${nameAnterior}`}</h3>
+            {albuns.map((value, id) => (
+              <Link
+                key={ id }
+                data-testid={ `link-to-album-${value.collectionId}` }
+                to={ `/album/${value.collectionId}` }
+              >
+                <div>
+                  <img src={ value.artworkUrl100 } alt={ value.collectionName } />
+                  <h5>{value.collectionName}</h5>
+                  <h6>{value.artistName}</h6>
+                </div>
+              </Link>
+            ))}
+          </div>
+        )}
       </div>
     );
   }
