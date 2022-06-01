81821ad03b31891e2c64f5da1910e315c5b68323
Gustavo Abreu <gustavofart371@gmail.com>
2022-06-01 12:03:15 -0300

    Cria o mecanismo para adicionar músicas na lista de músicas favoritas

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import getMusics from '../services/musicsAPI';
 import Carregando from '../pages/Carregando';
+import { addSong, getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 class MusicCard extends React.Component {
   constructor() {
@@ -9,6 +10,9 @@ class MusicCard extends React.Component {
     this.state = {
       objMusic: [],
       loading: true,
+      loadingFaroviteMusic: false,
+      favoriteSongs: [],
+      targetId: [],
     };
   }
 
@@ -22,7 +26,12 @@ class MusicCard extends React.Component {
   }
 
   render() {
-    const { objMusic, loading } = this.state;
+    const {
+      objMusic,
+      loading,
+      loadingFaroviteMusic,
+      favoriteSongs,
+      targetId } = this.state;
     return (
       <div>
         {loading ? (
@@ -31,18 +40,52 @@ class MusicCard extends React.Component {
           <div>
             <h4 data-testid="artist-name">{objMusic[0].artistName}</h4>
             <h5 data-testid="album-name">{objMusic[0].collectionName}</h5>
-            {objMusic.filter((value) => value.kind).map((value) => (
-              <div key={ value.trackId }>
-                <p>{value.trackName}</p>
-                <audio
-                  data-testid="audio-component"
-                  src={ value.previewUrl }
-                  controls
-                >
-                  <track kind="captions" />
-                </audio>
-              </div>
-            ))}
+            {loadingFaroviteMusic ? <Carregando /> : (
+              objMusic
+                .filter((value) => value.kind)
+                .map((value) => (
+                  <div key={ value.trackId }>
+                    <p>{value.trackName}</p>
+                    <audio
+                      data-testid="audio-component"
+                      src={ value.previewUrl }
+                      controls
+                    >
+                      <track kind="captions" />
+                    </audio>
+                    <label htmlFor="favorites-checkbox">
+                      Favorita
+                      <input
+                        data-testid={ `checkbox-music-${value.trackId}` }
+                        id={ value.trackId }
+                        type="checkbox"
+                        defaultChecked={ targetId.some((id) => id === value.trackId) }
+                        onClick={ ({ target }) => {
+                          const songs = objMusic.filter((song) => song.kind);
+                          const musicFavorite = songs.find((song) => (
+                            song.trackId === Number(target.id)
+                          ));
+                          targetId.push(Number(target.id));
+                          this.setState({
+                            loadingFaroviteMusic: true,
+                          }, async () => {
+                            if (target.checked) {
+                              await addSong(musicFavorite);
+                            }
+                            const favSongs = await getFavoriteSongs();
+                            this.setState({
+                              favoriteSongs: favSongs,
+                              loadingFaroviteMusic: false,
+                            }, () => {
+                            });
+                            console.log(favoriteSongs);
+                          });
+                        } }
+                      />
+                    </label>
+                  </div>
+                ))
+            )}
           </div>
         )}
       </div>
